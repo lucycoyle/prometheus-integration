02f4330591dcc3893adc9f6a0bf2a25f90defe13
unknown <lucy.coyle@mail.mcgill.ca>
2019-11-14 19:49:55 -0500

    added template code for nn and kn thinking

--- prometheus-ai/src/main/java/nn/internal/NeuralNetworkImpl.java
+++ prometheus-ai/src/main/java/nn/internal/NeuralNetworkImpl.java
@@ -4,13 +4,14 @@ import javax.inject.Inject;
 
 import interfaces.LayerOutput;
 import interfaces.SensorInput;
+import interfaces.Thinking;
 import interfaces.Tuple;
 import nn.api.NeuralNetwork;
 
 /**
  * Implementation of the NN.
  */
-class NeuralNetworkImpl implements NeuralNetwork, SensorInput, LayerOutput {
+class NeuralNetworkImpl implements NeuralNetwork, SensorInput, LayerOutput, Thinking {
     @Inject
     NeuralNetworkImpl() {
     }
@@ -20,4 +21,24 @@ class NeuralNetworkImpl implements NeuralNetwork, SensorInput, LayerOutput {
     public void receiveDataStream(int nnID, int nnStruct, int data[]) {
     	
     }
+    
+    public Tuple[] modifyOutputFormat(int[] data) {
+    	return new Tuple[10];
+    }
+    
+    public int[] nn(int[] input) {
+    	return input;
+    }
+    public Tuple[] think(int iterate, Tuple tuples[]) {
+    	int[] data = new int[1];					//TODO: find proper size for data array
+    	receiveDataStream(0, 0, data);				//TODO: find proper nnID and nnStruct values
+    	
+    	int[] nnOutput = nn(data);					//TODO: find real method signature for NN
+    	Tuple[] tnnOutput = modifyOutputFormat(nnOutput);
+    	for(Tuple t: tnnOutput) {
+    		sendDataStream(t);						
+    	}
+    	return tnnOutput;				
+    }
+    
 }
