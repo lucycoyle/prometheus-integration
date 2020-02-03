bb998a4b81cedffb6d4f48279ce2eae855580524
Reem Madkour <reem.madkour@mail.mcgill.ca>
2020-02-02 21:07:38 -0500

    added Tuples class

---
+++ prometheus-ai/src/main/java/interfaces/Tuples.java
@@ -0,0 +1,57 @@
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Tuples implements Iterable {
+
+	private ArrayList<Tuple> tupleList;
+
+
+	public Tuples(ArrayList<Tuple> input) {
+		this.tupleList = input;
+	}
+
+	public void add(Tuple t) {
+		tupleList.add(t);
+	}
+
+	@Override
+	public Iterator<Tuple> iterator() {
+		// TODO Auto-generated method stub
+		return new TuplesIterator(this);
+	}
+
+	class TuplesIterator implements Iterator<Tuple> {
+
+		private int ind;
+
+		// constructor
+		public TuplesIterator(Tuples tuples) {
+			// initialize cursor (return start pos)
+			this.ind = 0;
+
+		}
+
+		// Checks if the next element exists
+		@Override
+		public boolean hasNext() {
+			return (ind < tupleList.size());
+		}
+
+		// moves the cursor/iterator to next element
+		@Override
+		public Tuple next() {
+
+			Tuple thisTuple = tupleList.get(ind);
+			return thisTuple;
+
+		}
+
+		// Used to remove an element. Implemented only if needed
+		public void remove() {
+			// Default throws UnsupportedOperationException.
+		}
+	}
+
+}
